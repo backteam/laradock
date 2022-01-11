66914d1de1403ef538f548266e24417bf27e335f
mohammad amin packatchian fard <mapackatchian@gmail.com>
2022-01-12 01:36:10 +0330

    some change

---
+++ workspace/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '37b22ebe0121adba3ffdb1b1870f3fd98883bf92',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '37b22ebe0121adba3ffdb1b1870f3fd98883bf92',
+            'dev_requirement' => false,
+        ),
+    ),
+);
